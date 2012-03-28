6a5113284971a8eb37df0ec629bfeb84ff4466e0
Thomas Skardal <thomas.skardal@gmail.com>
2012-03-28 22:00:45 +0200

    fikset noen småting

--- src/main/scala/no/bouvet/bigone/AnagramFinder.scala
+++ src/main/scala/no/bouvet/bigone/AnagramFinder.scala
@@ -4,13 +4,8 @@ import collection.immutable.{Set, Map, List}
 
 
 object AnagramFinder {
-  def findLargestGroup(anagrams: Map[String, Set[String]]) = anagrams.maxBy(_._2.size)._2
-
-  def findLongest(anagrams: Map[String, Set[String]]) = anagrams.maxBy(_._1.length)._2
-
-
   def findAnagrams(words: List[String]) = {
-    var emptyMap = Map[String, Set[String]]() withDefaultValue Set[String]()
+    val emptyMap = Map[String, Set[String]]() withDefaultValue Set[String]()
 
     val anagrams = words.foldLeft(emptyMap) {
       (currentAnagrams, word) => addWord(currentAnagrams, word.trim.toLowerCase)
@@ -20,7 +15,11 @@ object AnagramFinder {
   }
 
   def addWord(currentAnagrams: Map[String, Set[String]], word: String) = {
-    var matches = currentAnagrams(word.sorted)
+    val matches = currentAnagrams(word.sorted)
     currentAnagrams + (word.sorted -> (matches + word))
   }
+
+  def findLargestGroup(anagrams: Map[String, Set[String]]) = anagrams.maxBy(_._2.size)._2
+
+  def findLongest(anagrams: Map[String, Set[String]]) = anagrams.maxBy(_._1.length)._2
 }
