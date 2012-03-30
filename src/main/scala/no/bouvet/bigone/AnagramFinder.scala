c80e1da382c6b51c1b1ae97853d6a33ffe1bfb78
Thomas Skardal <thomas.skardal@gmail.com>
2012-03-30 09:50:00 +0200

    live result

--- src/main/scala/no/bouvet/bigone/AnagramFinder.scala
+++ src/main/scala/no/bouvet/bigone/AnagramFinder.scala
@@ -3,5 +3,22 @@ package no.bouvet.bigone
 import collection.immutable.List
 
 object AnagramFinder {
-  def findAnagrams(words: List[String]) = List("pwned")
+
+  def findAnagrams(words: List[String]) : Map[String, Set[String]] = {
+    val initialAnagrams = Map[String, Set[String]]() withDefaultValue Set[String]()
+
+    val anagrams = words.foldLeft(initialAnagrams)(addWord)
+
+    anagrams.filter(tuple => tuple._2.size > 1)
+  }
+
+  def addWord(currentAnagrams: Map[String, Set[String]], w: String) = {
+    val newWord = w.trim.toLowerCase
+    var matches = currentAnagrams(newWord.sorted)
+    currentAnagrams + (newWord.sorted -> (matches + newWord))
+  }
+
+  def findLargestGroup(map: Map[String, Set[String]]) = map.maxBy(_._2.size)._2
+
+  def findLongest(map: Map[String, Set[String]]) = map.maxBy(_._1.length)._2
 }
--- src/test/scala/no/bouvet/bigone/AnagramKataSpec.scala
+++ src/test/scala/no/bouvet/bigone/AnagramKataSpec.scala
@@ -5,9 +5,30 @@ import org.specs2.mutable._
 
 class AnagramKataSpec extends Specification {
   "An anagram finder" should {
+
     "find zero anagrams in an empty list of words" in {
       val anagrams = AnagramFinder.findAnagrams(List[String]())
       anagrams must have size (0)
+    }
+
+    "find two anagrams in a list of three words where two consists of the same characters" in {
+      val anagrams = AnagramFinder.findAnagrams(List[String]("abc","xyz","cab"))
+      anagrams("abc") must contain("abc", "cab").only
+    }
+
+    "ignore words that have no anagrams" in {
+      val anagrams = AnagramFinder.findAnagrams(List[String]("abc","xyz","cab"))
+      anagrams("xyz") must have size(0)
+    }
+
+    "ignore case and leading/trailing whitespace" in {
+      val anagrams = AnagramFinder.findAnagrams(List[String](" abc","cAb "))
+      anagrams("abc") must contain ("abc", "cab")
+    }
+
+    "ignore duplicate words" in {
+      val anagrams = AnagramFinder.findAnagrams(List[String]("abc","abc", "cab"))
+        anagrams("abc") must contain ("abc", "cab").only
     }
   }
 }
